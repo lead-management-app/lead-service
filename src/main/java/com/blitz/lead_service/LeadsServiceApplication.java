360e188196d9ff4d6b5647012859c5062e5c235c
theKelvinCode <thekelvincode@gmail.com>
2025-10-06 19:07:31 +0100

    first commit

---
+++ src/main/java/com/blitz/lead_service/LeadsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blitz.lead_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blitz/lead_service/LeadsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blitz.lead_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
